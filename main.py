bd80e3e3da78b028cb1bfe517511ef0a32cb2aee
Nihit Jain <nihitjain11@gmail.com>
2019-05-18 15:53:31 +0530

    create html template

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,request,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Welcome to Homepage"
+@app.route("/<page>")
+def index(page=None):
+    return render_template("temp1.html",page=page)
+
+@app.route("/profile/<username>/<int:post_id>")
+def show_post(username,post_id):
+    return "Hello {} your post_id is {}".format(username,post_id)
 
 if __name__ == "__main__":
     app.run(debug=True)
